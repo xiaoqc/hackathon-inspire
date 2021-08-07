5cb07203b0a0c9b8b916d5f060dbb070e62455df
Wint Lu <xidian.lumeng@gmail.com>
2021-08-06 23:21:18 -0700

    additional fix for Stay1Page

--- src/scenes/Stay1SwipeToPDP.tsx
+++ src/scenes/Stay1SwipeToPDP.tsx
@@ -1,11 +1,9 @@
-import Stay1VideoPage from "../pages/Stay1VideoPage";
-import Stay1Pdp from "../pages/Stay1Pdp";
+import Stay1Page from "../pages/Stay1Page";
 
 export default function Stay1SwipeToPDP() {
   return (
     <>
-      <Stay1VideoPage />
-      <Stay1Pdp />
+      <Stay1Page />
     </>
   );
 }
