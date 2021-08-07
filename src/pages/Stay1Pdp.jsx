4be8ace5bfb28251c1ae58059de81a806d5117b1
Wint Lu <xidian.lumeng@gmail.com>
2021-08-06 18:44:35 -0700

    Add stay1 pdp page

---
+++ src/pages/Stay1Pdp.jsx
@@ -0,0 +1,9 @@
+import BasePage from "./BasePage";
+
+export default function Stay1Pdp() {
+  return (
+    <BasePage>
+      <img src="stay1-pdp-screen-shot.jpg" style={{ width: "100%" }} alt="" />
+    </BasePage>
+  );
+}
