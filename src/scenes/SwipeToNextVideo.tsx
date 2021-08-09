d40c366ae68ad48002fef186f54bf59f2bc0b239
Wint Lu <xidian.lumeng@gmail.com>
2021-08-08 19:04:08 -0700

    add additional

--- src/scenes/SwipeToNextVideo.tsx
+++ src/scenes/SwipeToNextVideo.tsx
@@ -1,17 +1,10 @@
-import { viewportWidth } from "../constants";
 import Stay1Page from "../pages/Stay1Page";
-
-const hScroller = {
-  width: viewportWidth
-};
 
 export default function SwipeToNextVideo() {
   return (
-    // <div className="h-scroller" style={hScroller}>
     <>
       <Stay1Page />
       <Stay1Page />
     </>
-    // </div>
   );
 }
